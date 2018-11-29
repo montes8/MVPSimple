9b27c735f2eb0f1839d0bbc085f4fe5216644117
montes8 <emontesinos812@gmail.com>
2018-11-28 22:20:17 -0500

    validacion con base de datos

--- app/src/main/java/com/example/tayler_gabbi/demo_mvp_simple/model/PresenterImpl.java
+++ app/src/main/java/com/example/tayler_gabbi/demo_mvp_simple/model/PresenterImpl.java
@@ -1,12 +1,18 @@
 package com.example.tayler_gabbi.demo_mvp_simple.model;
 
 import android.text.TextUtils;
+import android.util.Log;
+import com.example.tayler_gabbi.demo_mvp_simple.database.modelos.Usuario;
+import com.example.tayler_gabbi.demo_mvp_simple.database.modelos.UsuarioDao;
 import com.example.tayler_gabbi.demo_mvp_simple.presenter.LoginPresenter;
 import com.example.tayler_gabbi.demo_mvp_simple.view.LoginView;
+
+import java.util.ArrayList;
 
 public class PresenterImpl implements LoginPresenter {
 
     LoginView loginView;
+    private UsuarioDao usuarioDao;
 
     public PresenterImpl(LoginView loginView) {
         this.loginView = loginView;
@@ -21,6 +27,16 @@ public class PresenterImpl implements LoginPresenter {
 
         }else {
 
+            ArrayList<Usuario> listUsuario = (ArrayList<Usuario>) usuarioDao.queryBuilder().where(UsuarioDao.Properties.Usuario.eq(userName)).list();
+            Log.d("validaciologin",""+ listUsuario);
+            if(listUsuario!= null && listUsuario.size() > 0) {
+
+                loginView.loginSuccess();
+
+            } else {
+
+                loginView.loginError();
+            }
         }
     }
 
